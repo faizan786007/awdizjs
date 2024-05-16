01631f814dbc22ca981a0cae099818e1c5e3aaa5
faizan786007 <kingsmanfaizanali@gmail.com>
2024-05-16 17:37:18 +0530

    Create index.js
    settimeout AND setinterval

---
+++ day-16/index.js
@@ -0,0 +1,11 @@
+// console.log('before settimeout')
+
+// setTimeout(()=>{
+//     console.log('inside timeout function')
+// },8000)
+
+// console.log('after settimeout')
+
+setInterval(()=>{
+    console.log('inside setinterval')
+},2000)
